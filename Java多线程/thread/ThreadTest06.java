92dcfe95b3429e2f1ba948b2b10c7e81baed21b4
2985496686 <94234914+2985496686@users.noreply.github.com>
2022-05-22 22:10:29 +0800

    Add files via upload

--- Java多线程/thread/ThreadTest06.java
+++ Java多线程/thread/ThreadTest06.java
@@ -9,23 +9,22 @@ public class ThreadTest06 {
     public static void main(String[] args) {
         Thread t = new Thread(new myRunnable2());
         t.start();
-        try {
-            Thread.sleep(1000*3);
-            t.interrupt();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 }
 
 class myRunnable2 implements Runnable{
     public void run(){
-            System.out.println(Thread.currentThread().getName() + "------>start");
-            try {
-                Thread.sleep(1000*1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(Thread.currentThread().getName() + "------>end");
+        System.out.println(Thread.currentThread().getName() + "------>start");
+        System.out.println("中断前：" + Thread.currentThread().isInterrupted());
+        Thread.currentThread().interrupt();
+        System.out.println("中断后(遇到堵塞之前)：" + Thread.currentThread().isInterrupted());
+        System.out.println();
+        try {
+            Thread.sleep(1000*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("中断后(遇到堵塞之后)：" + Thread.currentThread().isInterrupted());
+        System.out.println(Thread.currentThread().getName() + "------>end");
     }
 }
