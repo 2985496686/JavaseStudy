70edc2804356e6f80c5546d49f33ae645edbe28e
2985496686 <94234914+2985496686@users.noreply.github.com>
2022-05-27 21:29:05 +0800

    Delete User.java

--- User.java
+++
@@ -1,8 +0,0 @@
-public class User {
-    //四个Field对象，分别采用四种不同的访问权限修饰符
-    public int no;
-    private String name;
-    protected  int age;
-    boolean sex;
-    public static final double MATH_PI = 3.1415926;
-}
