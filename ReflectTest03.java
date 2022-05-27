af2e8347038149c68238732b1dbe7e5c79ef6b9e
2985496686 <94234914+2985496686@users.noreply.github.com>
2022-05-27 21:28:17 +0800

    Delete ReflectTest03.java

--- ReflectTest03.java
+++
@@ -1,6 +0,0 @@
-public class ReflectTest03 {
-    public static void main(String[] args) throws ClassNotFoundException {
-        //forName()这个方法的执行会导致 MyClass 类加载，如果只想让一个类的静态代码块执行，就可以调用这个方法
-        Class c = Class.forName("MyClass");
-    }
-}
