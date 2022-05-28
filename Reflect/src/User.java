41d2d35208b80b72ae95718b39ecbb6cdc2e2c62
2985496686 <94234914+2985496686@users.noreply.github.com>
2022-05-28 22:14:53 +0800

    Add files via upload

--- Reflect/src/User.java
+++ Reflect/src/User.java
@@ -4,5 +4,24 @@ public class User {
     private String name;
     protected  int age;
     boolean sex;
+    public User(int no,String name,int age,boolean sex){
+        this.no = no;
+        this.age = age;
+        this.sex = sex;
+        this.name = name;
+    }
+    public User(){
+
+    }
     public static final double MATH_PI = 3.1415926;
+    public int getno(){
+        return no;
+    }
+    public void setno(int no){
+        this.no = no;
+    }
+    public int doSome(int i){
+        System.out.println("User对象中的doSome方法执行！");
+        return i;
+    }
 }
